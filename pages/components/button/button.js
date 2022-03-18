463182cb0f6e5fc43f08d1cde353e657662172f3
Alexjj_Melentyev <aleksei.melentev@coolclever.ru>
2022-03-18 14:47:34 +0300

    changed_btn

--- pages/components/button/button.js
+++ pages/components/button/button.js
@@ -1,43 +1,102 @@
-import style from "./button.module.scss";
+import style from "./P_Button.module.scss";
 import cn from "classnames";
+import Link from "next/link";
 
 const P_Button = ({
-  disabled = false,
   type = null,
+  disabled = false,
   text = null,
   size = null,
   icon = null,
   circle = null,
+  submit = null,
+  noTab = false,
+  href = "",
+  isLink = false,
+  styleInline = null,
+  ref = null,
+  color = null,
   onClick = () => {},
+  className = "",
+  target = null,
 }) => {
-  return (
-    <button
-      className={cn(style.main, {
-        //disabled
-        [style.disabled]: disabled,
-        //sizes
-        [style.lg]: size === "lg",
-        [style.md]: size === "md",
-        [style.sm]: size === "sm",
-        //types
-        [style.primary]: type === "primary",
-        [style.secondary]: type === "secondary",
-        [style.soft]: type === "soft",
-        [style.lilPrimary]: type === "lil-primary",
-        [style.ghost]: type === "ghost",
-        [style.critical]: type === "critical",
-        [style.lilCritical]: type === "lil-critical",
-        [style.circle]: circle === "true",
-        [style.smCircle]: circle === "true" && size === "sm"
-      })}
-      onClick={onClick}
-    >
-      {!!icon && (
-        <div className={cn(style.icon, { [style.noText]: !text })}>{icon}</div>
-      )}
-      <p>{text}</p>
-    </button>
-  );
+  if (!isLink) {
+    return (
+      <button
+        target={target}
+        ref={ref}
+        style={styleInline}
+        noTab={noTab ? -1 : 1}
+        type={submit}
+        className={cn(style.main, className, {
+          //disabled
+          [style.disabled]: disabled,
+          //sizes
+          [style.lg]: size === "lg",
+          [style.md]: size === "md",
+          [style.sm]: size === "sm",
+          //types
+          [style.primary]: type === "primary",
+          [style.secondary]: type === "secondary",
+          [style.soft]: type === "soft",
+          [style.lilPrimary]: type === "lil-primary",
+          [style.ghost]: type === "ghost",
+          [style.critical]: type === "critical",
+          [style.lilCritical]: type === "lil-critical",
+          [style.points]: type === "points",
+          [style.circle]: circle,
+          [style.smCircle]: circle && size === "sm",
+          [style.white]: color === "white",
+        })}
+        onClick={onClick}
+      >
+        {!!icon && (
+          <div className={cn(style.icon, { [style.noText]: !text })}>
+            {icon}
+          </div>
+        )}
+        <p>{text}</p>
+      </button>
+    );
+  } else {
+    return (
+      <Link href={href}>
+        <a
+          target={target}
+          style={styleInline}
+          noTab={noTab ? -1 : 1}
+          type={submit}
+          className={cn(style.main, {
+            //disabled
+            [style.disabled]: disabled,
+            //sizes
+            [style.lg]: size === "lg",
+            [style.md]: size === "md",
+            [style.sm]: size === "sm",
+            //types
+            [style.primary]: type === "primary",
+            [style.secondary]: type === "secondary",
+            [style.soft]: type === "soft",
+            [style.lilPrimary]: type === "lil-primary",
+            [style.ghost]: type === "ghost",
+            [style.critical]: type === "critical",
+            [style.lilCritical]: type === "lil-critical",
+            [style.points]: type === "points",
+            [style.circle]: circle,
+            [style.smCircle]: circle && size === "sm",
+          })}
+          onClick={onClick}
+        >
+          {!!icon && (
+            <div className={cn(style.icon, { [style.noText]: !text })}>
+              {icon}
+            </div>
+          )}
+          <p>{text}</p>
+        </a>
+      </Link>
+    );
+  }
 };
 
 export default P_Button;
